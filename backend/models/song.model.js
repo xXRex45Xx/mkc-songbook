ad12e0355689f919eb06b4e33f03b23af3c3cd70
xXRex45Xx <esrom.mulugeta@gmail.com>
2024-08-12 11:02:42 -0500

    Implemented inital song model

---
+++ backend/models/song.model.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+
+const songSchema = new Schema({
+    _id: { type: Number, required: true },
+    title: { type: String, required: true, index: true },
+    lyrics: { type: String, required: true },
+    musicElements: {
+        chords: { type: String },
+        tempo: { type: Number },
+        rythm: { type: String },
+    },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
+
+const SongModel = model("Song", songSchema);
+
+export default SongModel;
