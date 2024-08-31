750fa65f270223353eea1c155b0b04a03721ae0e
xXRex45Xx <esrom.mulugeta@gmail.com>
2024-08-31 16:32:37 -0500

    Implemented audio player slider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header.component";
+import PlayerContainer from "./components/player-container.component";
 
 function App() {
     return (
         <>
             <Header />
             <Outlet />
+            <PlayerContainer />
         </>
     );
 }
