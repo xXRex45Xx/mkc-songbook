3de9ce1746c1e3370b037a143992c1570afea774
abenezer legesse <abener.m27@gmail.com>
2024-09-30 21:34:42 -0500

    ablum route implementation fix

--- backend/index.js
+++ backend/index.js
@@ -10,26 +10,26 @@ if (config().error) throw config().error;
 const app = express();
 
 app.use(
-    cors({
-        origin: ["http://localhost:5173"],
-    })
+	cors({
+		origin: ["http://localhost:5173"],
+	})
 );
 app.use(express.json());
 app.use("/api", apiRouter);
 
 app.use(async (err, _req, res, _next) => {
-    let { message, statusCode = 500 } = err;
+	let { message, statusCode = 500 } = err;
 
-    if (statusCode === 500) {
-        console.error("Server error: ", err);
-        console.error("Internal error: ", err.internalError);
-        message = "An unexpected error occurred.";
-    }
-    res.status(statusCode).json({ message });
+	if (statusCode === 500) {
+		console.error("Server error: ", err);
+		console.error("Internal error: ", err.internalError);
+		message = "An unexpected error occurred.";
+	}
+	res.status(statusCode).json({ message });
 });
 
 connect(process.env.DB_URI).then(() => {
-    app.listen(process.env.PORT, () =>
-        console.log("Server started on port: ", process.env.PORT)
-    );
+	app.listen(process.env.PORT, () =>
+		console.log("Server started on port: ", process.env.PORT)
+	);
 });
--- backend/routes/album.route.js
+++ backend/routes/album.route.js
@@ -2,8 +2,12 @@ import { Router } from "express";
 import { getAllOrSearchAlbum } from "../controllers/album.controller.js";
 
 import { wrapAsync } from "../utils/error.util.js";
+import { validateGetAllAlbums } from "../middlewares/album-validation.middleware.js";
+
 const albumRouter = Router();
 
-albumRouter.route("/").get(wrapAsync(getAllOrSearchAlbum));
+albumRouter
+	.route("/")
+	.get(wrapAsync(validateGetAllAlbums), wrapAsync(getAllOrSearchAlbum));
 
 export default albumRouter;
