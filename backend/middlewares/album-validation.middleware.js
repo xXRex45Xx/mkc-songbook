c4789be0dbd2ad6e81f223949f9120ab09ebd2ce
abenezer legesse <abenezer.m27@gmail.com>
2024-10-02 16:59:24 -0500

    fixing alber valdation middleware error

--- backend/middlewares/album-validation.middleware.js
+++ backend/middlewares/album-validation.middleware.js
@@ -2,7 +2,7 @@ import { getAllAlbumQuerySchema } from "../models/validation-schemas/album.valid
 
 import validateSchema from "../utils/validator.util.js";
 
-export const validateGetAllAlbums = async (req, _res, next) => {
-	await validateSchema(req.query, getAllAlbumQuerySchema);
-	next();
-};
+// export const validateGetAllAlbums = async (req, _res, next) => {
+// 	await validateSchema(req.query, getAllAlbumQuerySchema);
+// 	next();
+// };
