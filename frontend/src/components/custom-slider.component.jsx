ae6b0d72ce6344ff7b64ef29ad892c2e1fbaa701
Esrom Tadesse <esrom@Esroms-MacBook-Air.local>
2025-11-01 21:11:12 -0500

    Fixed font size slider not working on mobile

--- frontend/src/components/custom-slider.component.jsx
+++ frontend/src/components/custom-slider.component.jsx
@@ -17,26 +17,28 @@ import { rangeSliderTheme } from "../config/forms-theme.config";
  * @param {boolean} [props.show] - Whether to show the slider
  */
 const CustomSlider = ({ value, step, min = 0, max, onChange, show }) => {
-    return (
-        <>
-            <RangeSlider
-                size="md"
-                value={value}
-                min={min}
-                max={max}
-                onChange={onChange}
-                theme={rangeSliderTheme}
-                step={step}
-                className="hidden md:flex"
-            />
-            <div
-                style={{
-                    width: ((value - min) * 100) / (max - min) + "%",
-                }}
-                className={`hidden md:block absolute left-0 -translate-y-1/2 h-2 bg-gradient-to-r from-secondary to-primary`}
-            ></div>
-        </>
-    );
+	return (
+		<>
+			<RangeSlider
+				size="md"
+				value={value}
+				min={min}
+				max={max}
+				onChange={onChange}
+				theme={rangeSliderTheme}
+				step={step}
+				className="hidden md:flex"
+			/>
+			<div
+				style={{
+					width: ((value - min) * 100) / (max - min) + "%",
+				}}
+				className={`${
+					show ? "block" : "hidden md:block"
+				} absolute left-0 -translate-y-1/2 h-2 bg-gradient-to-r from-secondary to-primary`}
+			></div>
+		</>
+	);
 };
 
 export default CustomSlider;
